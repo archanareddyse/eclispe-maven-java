9f206c81bc5ba1b974631a4917ffedfbc93c7bc1
archanase <archanareddycse@gmail.com>
2024-10-26 11:21:22 +0530

    push from eclispe

---
+++ src/test/java/com/kmit/a33/maven/java/AppTest.java
@@ -0,0 +1,19 @@
+package com.kmit.a33.maven.java;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
